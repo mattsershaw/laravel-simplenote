3eb870681779571fc38e7ed05d903e19db37349c
mattsershaw <masato.sasho@gmail.com>
2022-07-11 11:33:46 +0900

    request確認まで

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Memo;
+use App\Tag;
 
 class HomeController extends Controller
 {
@@ -24,5 +26,21 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function create()
+    {
+        // ログインしているユーザー情報をViewに渡す
+        $user = \Auth::user();
+        return view('create', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        dd($data);
+
+        // リダイレクト処理
+        return redirect()->route('home');
     }
 }
